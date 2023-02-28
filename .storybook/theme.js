f7990ff7cb454cf2c798014b95966cf68d6aec25
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-02-28 19:26:13 +0100

    fix: tailwind style

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,6 +2,6 @@ import { create } from "@storybook/theming";
 
 export default create({
   base: "light",
-  brandTitle: "form-atoms-field - atomic form library for react",
+  brandTitle: "@form-atoms/flowbite - Atomic Form Fields for Flowbite React",
   brandImage: "./form-atoms-logo.png",
 });
