4b772c43348f1b72e5fb00d8b13f310a35944b11
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-11-28 17:03:15 +0100

    test(path-alias): fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,10 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./setup.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
