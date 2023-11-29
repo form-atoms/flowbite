5c339cb2a19e6e127790cafac9082e1be64f4a43
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-11-29 09:55:02 +0100

    docs: drop styling addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,8 +6,7 @@ export default {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-styling",
+    "@storybook/addon-interactions"
   ],
   framework: { name: "@storybook/react-vite" },
   core: {
