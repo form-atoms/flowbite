22ea8ebf7788fbb8763fda7940888652ea30468a
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-10-20 13:32:17 +0200

    docs: update the list component showcase

--- src/components/list-field/Phones.stories.tsx
+++ src/components/list-field/Phones.stories.tsx
@@ -5,8 +5,8 @@ import {
   listField,
   textField,
 } from "@form-atoms/field";
-import { List } from "@form-atoms/list-atom";
-import { Card } from "flowbite-react";
+import { createList } from "@form-atoms/list-atom";
+import { Button, Card } from "flowbite-react";
 
 import { formStory, meta } from "../../stories/story-form";
 import { RadioOption } from "../radio-option";
@@ -23,41 +23,51 @@ const phones = listField({
     { number: "+421 933 888 999", primary: true },
     { number: "+420 905 100 200", primary: false },
   ],
-  fields: ({ number, primary }) => ({
-    number: textField({ name: "number", value: number }),
+  fields: () => ({
+    number: textField({ name: "number" }),
     primary: checkboxField({
       name: "primaryPhone",
-      value: primary,
     }).optional(),
   }),
 });
 
+const { List } = createList(phones);
+
 export const PhonesListField = formStory({
   args: {
     fields: { phones },
-    children: ({ required, fields }) => (
+    children: ({ required }) => (
       <RadioControl name="primaryPhone">
         {({ control }) => (
-          <List atom={fields.phones}>
-            {({ fields }) => (
-              <Card>
-                <TextField
-                  field={fields.number}
-                  required={required}
-                  label="Phone Number"
-                />
-                <Radio control={control} field={fields.primary}>
-                  {() => (
-                    <RadioOption
-                      field={fields.primary}
-                      required={required}
-                      label="Primary Phone"
-                      helperText="SMS to this phone will be used for authentication purposes"
-                    />
-                  )}
-                </Radio>
-              </Card>
-            )}
+          <List>
+            <List.Item>
+              {({ fields }) => (
+                <Card>
+                  <TextField
+                    field={fields.number}
+                    required={required}
+                    label="Phone Number"
+                  />
+                  <Radio control={control} field={fields.primary}>
+                    {() => (
+                      <RadioOption
+                        field={fields.primary}
+                        required={required}
+                        label="Primary Phone"
+                        helperText="SMS to this phone will be used for authentication purposes"
+                      />
+                    )}
+                  </Radio>
+                </Card>
+              )}
+            </List.Item>
+            <List.Add>
+              {({ add }) => (
+                <Button color="light" onClick={() => add()}>
+                  Add Phone
+                </Button>
+              )}
+            </List.Add>
           </List>
         )}
       </RadioControl>
